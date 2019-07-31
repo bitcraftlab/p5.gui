13877483af9a841cb3395c1f06c9cfda1657142f
Martin Schneider <craftoid@k2g2.org>
2019-07-31 03:17:48 +0200

    Add simple instance mode example

---
+++ examples/instance-mode/sketch.js
@@ -0,0 +1,33 @@
+let sketch = function(p) {
+
+  let div;
+
+  let params = {
+    r: 500
+  };
+
+  p.setup = function() {
+
+    div = p.canvas.parentElement;
+
+    p.createCanvas(div.offsetWidth, div.offsetHeight);
+    gui = p.createGui(this);
+    gui.addObject(params);
+  }
+
+  p.draw = function() {
+    p.background(220);
+    p.ellipse(p.width/2, p.height/2, params.r, params.r);
+  }
+
+  p.windowResized = function() {
+    p.resizeCanvas(div.clientWidth, div.clientHeight);
+  }
+
+}
+
+new p5(sketch, 'sketch1');
+new p5(sketch, 'sketch2');
+
+new p5(sketch, 'sketch3');
+new p5(sketch, 'sketch4');
